e00b8d5acf08d79cb7efe29fde604a99ea223680
lovedeep <lovedeepsingh9697@gmail.com>
2018-04-13 00:23:36 +0530

    html files& url mapping

--- Login_app/urls.py
+++ Login_app/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url,include
+from basic_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	url(r'^$', views.index,name='index'),
+    # path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^basic_app/',include(basic_app.urls))
+
 ]
